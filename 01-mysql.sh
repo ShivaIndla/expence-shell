c069564e0db1df113942cd2c6a02a0d112d82e3e
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-05 20:04:18 +0530

    for shell script

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -4,6 +4,31 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+RED="\e[31m"
+GREEN="\e[32m"
+NORMAL="\e[0m"
+
+VALIDATE(){
+
+    if[ $1 -eq 0]
+    then
+        echo -e " $GREEN $2  is allready installed  $Yellow ....Skipping $NORMAL"
+        echo "==================***=========================="
+    else
+        echo "installing $2 "
+        dnf install $2 -y
+        if [ $? -eq 0 ]
+        then
+            echo -e "Instalation of $Yellow $2 $NORMAL is $GREEN ....Success $NORMAL"
+            echo "==================***=========================="
+        else
+            echo -e "$RED error Please check the command $NORMAL" 
+
+            echo "==================***=========================="
+        fi
+
+    fi
+}
 
 
 if [ $USERID -ne 0 ]
@@ -13,3 +38,10 @@ then
 else
     echo " You are super user."
 fi
+
+for i in $@
+do
+ echo -e "Package is about to install: $GREEN $i $NORMAL"
+ dnf list installed $i
+VALIDATE $? $i
+done
