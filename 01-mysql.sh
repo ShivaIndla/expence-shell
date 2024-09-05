c84598e889aeb6a874aba1197508d323586e9d15
Shiva Indla <shiva.indla1519@gmail.com>
2024-09-05 19:37:00 +0530

    for shell script

---
+++ 01-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(ECHO $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+
+if [$USERID -ne 0]
+then
+    echo "PLease run this with root user!"
+    exit 1
+else
+    echo " You are super user."
+fi
